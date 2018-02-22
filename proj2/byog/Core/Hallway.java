3d0c748d9f39f122f757d730ed6756b755e4102d
Mingjun <limmingjun@berkeley.edu>
2018-02-22 15:20:39 -0800

    Initial draft of Hallway Class (Untested)

---
+++ proj2/byog/Core/Hallway.java
@@ -0,0 +1,54 @@
+package byog.Core;
+
+import byog.TileEngine.TETile;
+import byog.TileEngine.TERenderer;
+import byog.TileEngine.Tileset;
+
+public class Hallway {
+    /** Always plots left and upwards */
+    public TETile type;
+    public TETile[] horizontal;
+    public TETile[] vertical;
+    public int xInitial; /** the x coordinate of the middle of the lefter object */
+    public int yInitial; /** the y coordinate of the middle of the downer object */
+    private int xFinal;
+    private int yFinal;
+    public int xLength;
+    public int yLength;
+
+    /** Builds a hallway to be placed at location */
+    public Hallway(Room A, Room B, TETile type) {
+            xInitial = Math.min(A.center[0], B.center[0]);
+            yInitial = Math.min(A.center[1], B.center[1]);
+            xFinal = Math.max(A.center[0], B.center[0]);
+            yFinal = Math.max(A.center[1], B.center[1]);
+            xLength = xFinal-xInitial;
+            yLength = yFinal-yLength;
+            this.type = type;
+            horizontal = new TETile[xLength];
+            for (int i = 0; i < xLength; i++) {
+                horizontal[i] = type;
+            }
+            vertical = new TETile[yLength];
+            for (int i = 0; i < yLength; i++) {
+                vertical[i] = type;
+            }
+        }
+    public Hallway(Room A, Room B) {
+        xInitial = Math.min(A.center[0], B.center[0]);
+        yInitial = Math.min(A.center[1], B.center[1]);
+        xFinal = Math.max(A.center[0], B.center[0]);
+        yFinal = Math.max(A.center[1], B.center[1]);
+        xLength = xFinal-xInitial;
+        yLength = yFinal-yLength;
+        this.type = Tileset.FLOOR;
+        horizontal = new TETile[xLength];
+        for (int i = 0; i < xLength; i++) {
+            horizontal[i] = Tileset.FLOOR;
+        }
+        vertical = new TETile[yLength];
+        for (int i = 0; i < yLength; i++) {
+            vertical[i] = Tileset.FLOOR;
+        }
+    }
+}
