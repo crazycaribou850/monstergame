3d47ed9692c55f892b5428b8ce1571aa74c51f55
michelle54725 <michelle54725@gmail.com>
2018-02-22 20:22:42 -0800

    initial Room class (tested); added Tests package, TestRoom.java

---
+++ proj2/byog/Core/Notes.java
@@ -0,0 +1,34 @@
+package byog.Core;
+/**
+ * This file does not contribute to the program.
+ * These are notes and notes only to assist in communication between creators
+ * */
+
+/** Mao 02/22/18 ~7pm
+
+ * Assigned variable name to longitude instead of long because long is primitive type
+
+ * RandomUtils are not miscellaneous utilities. They are utilities for randomness.
+
+ * If width/height is even, then true middle will be in between two tiles
+ * Current solution: whatever floor division gives (Java / = Python //)
+ *  e.g. width = 5
+ *   - - - - -
+ *   - - - - -
+ *   - - - - -
+ *       ^ 5/2 = 2 -> center[0] = lat + 2
+ *
+ *  e.g. width = 4
+ *  - - - - - -
+ *  - - - - - -
+ *  - - - - - -
+ *        ^ 6/2 = 3 -> center[0] = lat + 3
+
+ * Exceptions to catch/throw:
+ * 1. Creating Room with width/height <=2 (just walls)
+ *
+
+ */
+
+public class Notes {
+}
---
+++ proj2/byog/Core/Room.java
@@ -0,0 +1,63 @@
+package byog.Core;
+
+import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
+
+public class Room {
+    public TETile[][] room;
+    public TETile Ptype; /** Tile type for room's perimeter (default: WALL) */
+    public TETile type; /** Tile type for room's floors (default: FLOOR) */
+
+    public int width;
+    public int height;
+
+    /** Bottom left */
+    public int latitude;
+    public int longitude;
+
+    public int[] center; /** center[0], center[1] = latitude, longitude of center */
+
+    public Room(int w, int h, int lat, int lng) {
+        Ptype = Tileset.WALL;
+        type = Tileset.FLOOR;
+
+        width = w;
+        height = h;
+
+        latitude = lat;
+        longitude = lng;
+
+        center = new int[2];
+        center[0] = lat + w/2;
+        center[1] = lng + h/2;
+
+        room = buildRoom(w, h);
+    }
+
+    private TETile[][] buildRoom(int w, int h) {
+        TETile[][] room = new TETile[w][h];
+
+        for (int x = 0; x < w; x += 1) {
+            for (int y = 1; y < h; y += 1) {
+                room[x][y] = this.type;
+            }
+        }
+
+        /** Top & Bottom Walls */
+        for (int x = 0; x < w; x += 1) {
+            room[x][0] = this.Ptype;
+            room[x][h - 1] = this.Ptype;
+        }
+
+        /** Left & Right Walls */
+        for (int y = 0; y < h; y += 1) {
+            room[0][y] = this.Ptype;
+            room[w - 1][y] = this.Ptype;
+        }
+
+        return room;
+    }
+}
+
+
+
