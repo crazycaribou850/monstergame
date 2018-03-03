6c442089ed25c543898c462332c5b5fa954b4970
Mingjun <limmingjun@berkeley.edu>
2018-03-03 07:43:20 -0800

    made everything a really ugly color

--- proj2/byog/TileEngine/Tileset.java
+++ proj2/byog/TileEngine/Tileset.java
@@ -17,10 +17,10 @@ import java.awt.Color;
  */
 
 public class Tileset {
-    public static final TETile PLAYER = new TETile('@', Color.white, Color.black, "player");
+    public static final TETile PLAYER = new TETile('@', Color.blue, new Color(128, 192, 128), "player");
     public static final TETile WALL = new TETile('#', new Color(216, 128, 128), Color.darkGray,
             "wall");
-    public static final TETile FLOOR = new TETile('·', new Color(128, 192, 128), Color.black,
+    public static final TETile FLOOR = new TETile('·', new Color(128, 192, 128), new Color(128, 192, 128),
             "floor");
     public static final TETile NOTHING = new TETile(' ', Color.black, Color.black, "nothing");
     public static final TETile GRASS = new TETile('"', Color.green, Color.black, "grass");
@@ -33,7 +33,7 @@ public class Tileset {
     public static final TETile SAND = new TETile('▒', Color.yellow, Color.black, "sand");
     public static final TETile MOUNTAIN = new TETile('▲', Color.gray, Color.black, "mountain");
     public static final TETile TREE = new TETile('♠', Color.green, Color.black, "tree");
-    public static final TETile COIN = new TETile('0', Color.yellow, Color.black, "coin");
+    public static final TETile COIN = new TETile('0', Color.yellow, new Color(128, 192, 128), "coin");
     public static final TETile MONSTER = new TETile('♠', Color.black, Color.red, "IM GONNA GET U");
 }
 
